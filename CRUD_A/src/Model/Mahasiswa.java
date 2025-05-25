d67a6ecca0544e728d08cb49ace627fd7a565596
madityanurdiansyah <karangjatic713@gmail.com>
2025-05-25 17:43:57 +0700

    Update Mahasiswa.java

--- CRUD_A/src/Model/Mahasiswa.java
+++ CRUD_A/src/Model/Mahasiswa.java
@@ -13,7 +13,6 @@ public class Mahasiswa {
     private String nama;
     private String nim;
 
-    // Getters & Setters
     public int getId() { return id; }
     public void setId(int id) { this.id = id; }
     public String getNama() { return nama; }
